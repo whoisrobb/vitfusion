34a4a56b21bcac9059824d27a4f05fe2e3789d73
Muchiri <developedbyrobbie@gmail.com>
2024-08-27 10:29:31 +0300

    feat: add lenis smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import { Route, Routes } from "react-router-dom"
-import Layout from "./app/layout"
-import Home from "./app/home"
+import { Route, Routes } from "react-router-dom";
+import Layout from "./app/layout";
+import Home from "./app/home";
+import { useEffect } from "react";
+import Lenis from 'lenis';
+import 'lenis/dist/lenis.css';
 
 const App = () => {
+
+  useEffect(() => {
+    const lenis = new Lenis()
+    
+    function raf(time: any) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf)
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
